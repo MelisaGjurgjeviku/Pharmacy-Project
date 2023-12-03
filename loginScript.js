79e0009d2ba3beef1dc3436d154ecf7fe3d4488c
MelisaGjurgjeviku <112248470+MelisaGjurgjeviku@users.noreply.github.com>
2023-12-03 01:05:58 +0100

    javascript validimi per login and register formen

---
+++ loginScript.js
@@ -0,0 +1,24 @@
+function validateLoginForm() {
+    var username = document.getElementById('loginUsername').value;
+    var password = document.getElementById('loginPassword').value;
+
+    if (username === '' || password === '') {
+        alert('Please enter both username and password.');
+        return false;
+    }
+
+    return true;
+}
+
+function validateRegisterForm() {
+    var username = document.getElementById('registerUsername').value;
+    var email = document.getElementById('registerEmail').value;
+    var password = document.getElementById('registerPassword').value;
+
+    if (username === '' || email === '' || password === '') {
+        alert('Please fill in all fields.');
+        return false;
+    }
+
+    return true;
+}
