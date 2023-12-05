0f05aac6c87509a0fb0c0b676c7c1ee3a81ec81e
Mirelabeka <149654933+Mirelabeka@users.noreply.github.com>
2023-12-05 21:33:05 +0200

    validimi js

---
+++ contact.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('Form');
+const Name = document.getElementById('Name');
+const Phone = document.getElementById('Phone');
+const Email = document.getElementById('Email');
+const Subject= document.getElementById('Subject');
+
+
+
